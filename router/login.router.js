fd171f94ae9f404bf292ad4e9545c804da0036dc
Sumanbitw <007sumanghosh@gmail.com>
2021-06-13 16:28:33 +0530

    login

--- router/login.router.js
+++ router/login.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../model/user-models")
 
 router.post("/", async (req, res) => {
     const { email, password } = req.body
--- router/signup.router.js
+++ router/signup.router.js
@@ -31,7 +31,7 @@ router.post("/", async (req, res) => {
   const newUser = new User({
     name: req.body.name,
     email: req.body.email,
-    password: hashedPassword,
+    password: req.body.password,
   });
   try {
     const savedUser = await newUser.save();
