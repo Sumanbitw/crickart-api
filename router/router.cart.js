b27594e03f56e32aa26c49094c91057fb3c35350
Sumanbitw <007sumanghosh@gmail.com>
2021-06-16 23:23:59 +0530

    refactor

--- router/router.cart.js
+++ router/router.cart.js
@@ -33,7 +33,7 @@ router.post('/:userId/:productId', async ( req, res) => {
     const { userId, productId } = req.params
     const { quantity } = req.body
     try{
-        const updatedCart = quantity === 1 ? await cartModels.remove({ user : { _id : userId }, product : {_id : productId }})
+        const updatedCart = quantity > 1 ? await cartModels.remove({ user : { _id : userId }, product : {_id : productId }})
         : await cartModels.updateOne({ user : { _id : userId }, product : { _id : productId }}, { quantity })
         res.json({ success : true, updatedCart : updatedCart, message : "Items added in cart "})
     }catch(error){
