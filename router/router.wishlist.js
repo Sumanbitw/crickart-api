71662dfbfd466d6e09ba36b9bfe480afd1837ffc
Sumanbitw <007sumanghosh@gmail.com>
2021-06-17 16:35:40 +0530

    refactor wishlist

--- router/router.wishlist.js
+++ router/router.wishlist.js
@@ -15,9 +15,9 @@ router.get("/:userId", async ( req,res ) => {
 })
 
 router.post("/", async (req,res) => {
+    const { userId, productId } = req.params
     try {
-        const newWishlist = new wishlistModels(req.body)
-        const savedWishlist = await newWishlist.save()
+        const savedWishlist = await wishlistModels({user : userId, product : productId }).save()
         res.status(201).json({wishlist : savedWishlist, success:true, message : "Added in wishlist"})
     }catch(error){
         res.status(500).json({
