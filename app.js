8d9cede62ca051a8fc7a2da2a44f70a1d6427dac
Sumanbitw <007sumanghosh@gmail.com>
2021-07-22 16:59:49 +0530

    implemented stripe functionality

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express()
 const mongoose = require("mongoose")
 const cors = require("cors")
 require("dotenv/config")
+
+const stripe = require("stripe")(process.env.SECRET_KEY);
+const uuid = require("uuid");
+
 const port = process.env.PORT || 3010
 
 app.use(cors())
@@ -22,6 +26,39 @@ app.get("/",(req,res) => {
     res.send("Ecommerce backeend")
 })
 
+app.post("/payment", (req, res) => {
+    const { product, token } = req.body;
+    console.log("PRODUCT ", product);
+    console.log("PRICE ", product.price);
+    const idempontencyKey = uuid();
+  
+    return stripe.customers
+      .create({
+        email: token.email,
+        source: token.id
+      })
+      .then(customer => {
+        stripe.charges.create(
+          {
+            amount: product.price * 100,
+            currency: "usd",
+            customer: customer.id,
+            receipt_email: token.email,
+            description: `purchase of ${product.name}`,
+            shipping: {
+              name: token.card.name,
+              address: {
+                country: token.card.address_country
+              }
+            }
+          },
+          { idempontencyKey }
+        );
+      })
+      .then(result => res.status(200).json(result))
+      .catch(err => console.log(err));
+  });
+  
 mongoose.connect(process.env.DB_CONNECTION,{useNewUrlParser:true, useUnifiedTopology:true}, () =>{
     console.log("connected to db")
 })
@@ -30,5 +67,7 @@ app.use("/cart", cartRoute)
 app.use("/wishlist", wishlistRoute)
 app.use("/signup", signupRoute)
 app.use("/login", loginRoute)
+
+
 
 app.listen(port)
--- router/login.router.js
+++ router/login.router.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs')
+var jwt = require('jsonwebtoken');
+require("dotenv/config")
 const User = require("../model/user-models")
 
 router.post("/", async (req, res) => {
@@ -12,6 +15,25 @@ router.post("/", async (req, res) => {
     }
     res.json({ user:null, success: false, message : "No account exist with the entered email id" })
   })
+
+  // router.post('/login',async (req, res, next) => {
+  //   const user = await User.findOne({ email : req.body.email })
+  //   !user && res.status(401).json({ message : "Auth login failed" })
+    
+  //   const validatePassword = await bcrypt.compare(req.body.password, user.password)
+  //   !validatePassword && res.status(401).json({ message : "Wrong Password" })
+    
+  //   try{
+  //       let token = jwt.sign({ userId : user._id, email : user.email }, 
+  //       process.env.JWT_KEY, {expiresIn : "1d"})
+    
+  //       token=`Bearer ${token}`;
+  //       res.status(200).json({ message : "Succesfully login", user : user, token : token})
+  //       }catch(error) {
+  //           res.status(500).json({ message : error })
+  //       }
+  //   })
+    
   
   
   module.exports = router
